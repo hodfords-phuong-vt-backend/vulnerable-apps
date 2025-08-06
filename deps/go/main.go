7fdc633cadce5a71e3b628edec89a2c8e20370d3
hodfords-phuong-vo-be <phuong.vt@hodfords.com>
2025-08-06 13:26:15 +0700

    feat(deps): update go

--- deps/go/main.go
+++ deps/go/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
     "fmt"
+
+    _ "github.com/gin-gonic/gin"
+    _ "github.com/gorilla/websocket"
+    _ "gopkg.in/yaml.v2"
+    _ "github.com/dgrijalva/jwt-go"
 )
 
 func main() {
